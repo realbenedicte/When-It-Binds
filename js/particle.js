290b310ed178e6cabfce20cfd968de204358958f
andreiv <andreiv@propertyvista.com>
2021-02-21 12:35:11 -0500

    added gravity points

--- js/particle.js
+++ js/particle.js
@@ -163,6 +163,154 @@ Particle.prototype = (function (o) {
   },
 })
 
+
+
+/**
+ * GravityPoint
+ */
+function GravityPoint(x, y, radius, targets) {
+  Vector.call(this, x, y);
+  this.radius = radius;
+  this.currentRadius = radius * 0.5;
+
+  this._targets = {
+    particles: targets.particles || [],
+    gravities: targets.gravities || [],
+  };
+  this._speed = new Vector();
+}
+
+GravityPoint.RADIUS_LIMIT = 65;
+GravityPoint.interferenceToPoint = true;
+
+GravityPoint.prototype = (function (o) {
+  var s = new Vector(0, 0), p;
+  for (p in o) s[p] = o[p];
+  return s;
+})({
+  gravity: 0.05,
+  isMouseOver: false,
+  dragging: false,
+  destroyed: false,
+  _easeRadius: 0,
+  _dragDistance: null,
+  _collapsing: false,
+
+  hitTest: function (p) {
+    return this.distanceTo(p) < this.radius;
+  },
+
+  startDrag: function (dragStartPoint) {
+    this._dragDistance = Vector.sub(dragStartPoint, this);
+    this.dragging = true;
+  },
+
+  drag: function (dragToPoint) {
+    this.x = dragToPoint.x - this._dragDistance.x;
+    this.y = dragToPoint.y - this._dragDistance.y;
+  },
+
+  endDrag: function () {
+    this._dragDistance = null;
+    this.dragging = false;
+  },
+
+  addSpeed: function (d) {
+    this._speed = this._speed.add(d);
+  },
+
+  collapse: function (e) {
+    this.currentRadius *= 1.75;
+    this._collapsing = true;
+  },
+
+  render: function (gp) {
+    if (this.destroyed) return;
+
+    var particles = this._targets.particles,
+      i, len;
+
+    for (i = 0, len = particles.length; i < len; i++) {
+      particles[i].addSpeed(Vector.sub(this, particles[i]).normalize().scale(this.gravity));
+    }
+
+    this._easeRadius = (this._easeRadius + (this.radius - this.currentRadius) * 0.07) * 0.95;
+    this.currentRadius += this._easeRadius;
+    if (this.currentRadius < 0) this.currentRadius = 0;
+
+    if (this._collapsing) {
+      this.radius *= 0.75;
+      if (this.currentRadius < 1) this.destroyed = true;
+      this._draw(gp)
+      return;
+    }
+
+    var gravities = this._targets.gravities,
+      gps = this._targets.gps,
+      g, absorp,
+      area = this.radius * this.radius * Math.PI, garea;
+
+    for (i = 0, len = gravities.length; i < len; i++) {
+      g = gravities[i];
+      if (g === this || g.destroyed) continue;
+
+      if (
+        (this.currentRadius >= g.radius || this.dragging) &&
+        this.distanceTo(g) < (this.currentRadius + g.radius) * 0.85
+      ) {
+        g.destroyed = true;
+        this.gravity += g.gravity;
+
+        absorp = Vector.sub(g, this).scale(g.radius / this.radius * 0.5);
+        this.addSpeed(absorp);
+
+        garea = g.radius * g.radius * Math.PI;
+        this.currentRadius = Math.sqrt((area + garea * 3) / Math.PI);
+        this.radius = Math.sqrt((area + garea) / Math.PI);
+      }
+
+      g.addSpeed(Vector.sub(this, g).normalize().scale(this.gravity));
+    }
+
+    if (GravityPoint.interferenceToPoint && !this.dragging)
+      this.add(this._speed);
+
+    this._speed = new Vector();
+
+    if (this.currentRadius > GravityPoint.RADIUS_LIMIT) this.collapse();
+
+    this._draw(gp);
+  },
+
+  _draw: function (gp) {
+    var grd, r;
+
+    gp.setAttribute("style", `transform: translate(${this.x}px, ${this.y}px) scale(${this.radius / 5}, ${this.radius / 5});`)
+    // ctx.save();
+
+    // grd = ctx.createRadialGradient(this.x, this.y, this.radius, this.x, this.y, this.radius * 5);
+    // grd.addColorStop(0, 'rgba(0, 0, 0, 0.1)');
+    // grd.addColorStop(1, 'rgba(0, 0, 0, 0)');
+    // ctx.beginPath();
+    // ctx.arc(this.x, this.y, this.radius * 5, 0, Math.PI * 2, false);
+    // ctx.fillStyle = grd;
+    // ctx.fill();
+
+    // r = Math.random() * this.currentRadius * 0.7 + this.currentRadius * 0.3;
+    // grd = ctx.createRadialGradient(this.x, this.y, r, this.x, this.y, this.currentRadius);
+    // grd.addColorStop(0, 'rgba(0, 0, 0, 1)');
+    // grd.addColorStop(1, Math.random() < 0.2 ? 'rgba(255, 196, 0, 0.15)' : 'rgba(103, 181, 191, 0.75)');
+    // ctx.beginPath();
+    // ctx.arc(this.x, this.y, this.currentRadius, 0, Math.PI * 2, false);
+    // ctx.fillStyle = grd;
+    // ctx.fill();
+    // ctx.restore();
+  }
+});
+
+
+
+
 // Initialize
 const test = (() => {
 
@@ -179,7 +327,8 @@ const test = (() => {
   var mouse = new Vector(),
     gravities = [],
     particles = [],
-    imgs = [];
+    imgs = [],
+    gps = [];
 
 
   // Event Listeners
@@ -199,6 +348,7 @@ const test = (() => {
   function mouseMove(e) {
     mouse.set(e.clientX, e.clientY);
 
+    console.log("got mousex mousey", e.clientX, e.clientY)
     // var i, g, hit = false;
     // for (i = gravities.length - 1; i >= 0; i--) {
     //   g = gravities[i];
@@ -211,18 +361,27 @@ const test = (() => {
     // canvas.style.cursor = hit ? 'pointer' : 'default';
   }
 
-  // function mouseDown(e) {
-  //   for (var i = gravities.length - 1; i >= 0; i--) {
-  //     if (gravities[i].isMouseOver) {
-  //       gravities[i].startDrag(mouse);
-  //       return;
-  //     }
-  //   }
-  //   gravities.push(new GravityPoint(e.clientX, e.clientY, G_POINT_RADIUS, {
-  //     particles: particles,
-  //     gravities: gravities
-  //   }));
-  // }
+  function mouseDown(e) {
+    // for (var i = gravities.length - 1; i >= 0; i--) {
+    //   if (gravities[i].isMouseOver) {
+    //     gravities[i].startDrag(mouse);
+    //     return;
+    //   }
+    // }
+
+    console.log(" MOUSE DOWN ", e.clientX, e.clientY)
+    let gp = document.createElement('div');
+    gp.className = "gp"
+    gp.setAttribute("style", `transform: translate(${e.clientX}px, ${e.clientY}px);`)
+    document.body.appendChild(gp)
+    gps.push(gp)
+
+    gravities.push(new GravityPoint(e.clientX, e.clientY, G_POINT_RADIUS, {
+      particles: particles,
+      gravities: gravities
+    }));
+
+  }
 
   // function mouseUp(e) {
   //   for (var i = 0, len = gravities.length; i < len; i++) {
@@ -258,18 +417,23 @@ const test = (() => {
   }
 
 
-  // Init
-  window.addEventListener('resize', resize, false);
+
+
   resize(null);
 
   imgs = document.getElementsByClassName("left")
   addParticle(imgs);
   window.addEventListener('mousemove', mouseMove, false);
+  // Init
+  window.addEventListener('resize', resize, false);
+  window.addEventListener('mousedown', mouseDown, false);
+  // canvas.addEventListener('mouseup', mouseUp, false);
+  // canvas.addEventListener('dblclick', doubleClick, false);
 
   // Start Update
 
   var loop = function () {
-    var i, len, g, p;
+    var i, len, g, p, context;
 
     // context.save();
     // context.fillStyle = BACKGROUND_COLOR;
@@ -288,6 +452,22 @@ const test = (() => {
     //     i--;
     //   }
     // }
+
+    for (i = 0, len = gravities.length; i < len; i++) {
+      g = gravities[i];
+      gp = gps[i];
+      if (g.dragging) g.drag(mouse);
+      g.render(gp);
+
+      if (g.destroyed) {
+        gravities.splice(i, 1);
+        gps.splice(i, 1)
+        gp.remove()
+        len--;
+        i--;
+      }
+    }
+
 
     // bufferCtx.save();
     // bufferCtx.globalCompositeOperation = 'destination-out';
